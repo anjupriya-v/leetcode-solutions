6761c9afaa05348c61a65b67bba6b96aaa96e074
Anju Priya V <84177086+anjupriya-v@users.noreply.github.com>
2022-08-06 19:12:15 +0530

    Create MirrorReflection.java

---
+++ Math/MirrorReflection.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        int ext=q;
+        int reflection=p;
+        while(ext % 2==0 && reflection %2==0){
+            ext/=2;
+            reflection/=2;
+        }
+        if(ext%2 ==0 && reflection%2 !=0){
+            return 0;
+        }
+        if(ext%2 !=0 && reflection%2 ==0){
+            return 2;
+        }
+        if(ext%2 !=0 && reflection%2 !=0){
+            return 1;
+        }
+        return -1;
+    }
+}
